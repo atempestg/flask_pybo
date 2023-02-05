73e113bf54e0029828eb25480ee5378c54a825ed
atempestg <atempestg@gmail.com>
2023-02-05 15:28:57 +0900

    20230205_03

---
+++ pybo/bat.cmd
@@ -0,0 +1,6 @@
+@echo off
+cd C:\git_workspace\Workspace\pybo
+set FLASK_APP=pybo
+set FLASK_ENV=development
+set APP_CONFIG_FILE=c:\git_workspace\Workspace\pybo\pybo\config\development.py
+C:\venvs\myproject\Scripts\activate
