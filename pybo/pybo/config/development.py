620b6852f399bbe86a3946cda6691ef54dcb8310
atempestg <atempestg@gmail.com>
2023-02-06 21:20:43 +0900

    20230206_01

--- pybo/pybo/config/development.py
+++ pybo/pybo/config/development.py
@@ -1,5 +1,5 @@
 from pybo.config.default import *
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo_bak.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = "dev"
