706ad73a4da712acd28e72a2a79f21760320384d
atempestg <atempestg@gmail.com>
2023-02-19 19:16:48 +0900

    '20230219_01'

---
+++ pybo/myproject.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+
+cd ~/projects/flask_pybo/pybo
+export FLASK_APP=pybo
+export FLASK_ENV=development
+export APP_CONFIG_FILE=/home/ubuntu/projects/flask_pybo/pybo/pybo/config/production.py
+. ~/venvs/myproject/bin/activate
