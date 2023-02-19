608a4a239a4ac8453916f65320f4a0f384f420a6
atempestg <atempestg@gmail.com>
2023-02-19 23:08:35 +0900

    20230219_06

--- pybo/pybo/views/main_views.py
+++ pybo/pybo/views/main_views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, url_for
+from flask import Blueprint, render_template, url_for, current_app
 from werkzeug.utils import redirect
 
 from pybo.models import Question
@@ -8,4 +8,5 @@ bp = Blueprint('main',__name__, url_prefix='/')
 
 @bp.route('/')
 def index():
+    current_app.logger.info("INFO 레벨로 출력")
     return redirect(url_for('question._list'))
